444666a520848c2ee4733f6ea73a9a9e60cf633e
Tangui Le Pense <29804907+tanguilp@users.noreply.github.com>
2023-06-19 16:36:32 +0200

    Create project

---
+++ lib/req_http_cache.ex
@@ -0,0 +1,18 @@
+defmodule ReqHTTPCache do
+  @moduledoc """
+  Documentation for `ReqHTTPCache`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ReqHTTPCache.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/req_http_cache_test.exs
@@ -0,0 +1,8 @@
+defmodule ReqHTTPCacheTest do
+  use ExUnit.Case
+  doctest ReqHTTPCache
+
+  test "greets the world" do
+    assert ReqHTTPCache.hello() == :world
+  end
+end
